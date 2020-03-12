899f4eb4ce785c43258c982fa4048571ca1f627f
Кудрин Павел Константинович <pkkudrin@gmail.com>
2020-03-12 23:04:48 +0700

    Upload Project and Simple Scene Managament

---
+++ Assets/SceneManagament/Examples/Scripts/Scripts/ExampleScene.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SceneManagament.Examples
+{
+    public class ExampleScene: Scene
+    {
+        private const string SCENE_NAME = "DefaultScene";
+
+        protected override string SceneId => SCENE_NAME;
+    }
+}
---
+++ Assets/SceneManagament/Scripts/PlatformScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SceneManagament
+{
+    public abstract class PlatformScene : Scene
+    {
+        protected abstract string IosSceneName { get; }
+        protected abstract string AndroidSceneName { get; }
+        protected abstract string WindowsSceneName { get; }
+        protected abstract string DefaultSceneName { get; }
+
+        protected sealed override string SceneId
+        {
+            get
+            {
+                switch (Application.platform)
+                {
+                    case RuntimePlatform.Android:
+                        return AndroidSceneName;
+                    case RuntimePlatform.IPhonePlayer:
+                        return IosSceneName;
+                    case RuntimePlatform.WSAPlayerX86:
+                    case RuntimePlatform.WSAPlayerX64:
+                    case RuntimePlatform.WindowsEditor:
+                        return WindowsSceneName;
+                    default:
+                        return DefaultSceneName;
+                }
+            }
+        }
+    }
+}
