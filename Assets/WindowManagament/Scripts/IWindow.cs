7a1a9705d5556f0d1daa60a03f2b1b1c3aa2f05e
Кудрин Павел Константинович <pkkudrin@gmail.com>
2020-03-15 16:36:54 +0700

    WindowManager v1.0.0

---
+++ Assets/WindowManagament/Scripts/IWindow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace WindowManagament
+{
+    public interface IWindow<TWindowContext> where TWindowContext: IWindowContext
+    {
+        Transform Parent { set; }
+
+        TWindowContext Context { set; }
+
+        void ShowWindow();
+
+        void HideWindow();
+
+        void CloseWindow();
+
+        void Reset();
+    }
+}
