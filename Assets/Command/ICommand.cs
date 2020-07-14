7a33689fd940dfb41d1da971b0ef49b9e89837e6
meduzmi <pkkudrin@gmail.com>
2020-07-14 06:08:01 +0300

    fast add command observer combination

---
+++ Assets/Command/ICommand.cs
@@ -0,0 +1,13 @@
+﻿// MIT LICENSE
+// https://github.com/medzumi/UnityPlugins/blob/master/LICENSE
+
+/// <summary>
+/// Интерфейс команды
+/// </summary>
+public interface ICommand
+{
+    /// <summary>
+    /// Исполнить команду
+    /// </summary>
+    void Execute();
+}
