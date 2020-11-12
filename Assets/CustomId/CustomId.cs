d350530b996c80bec158ba091f3cbbd6011f689c
medzumi <40718770+medzumi@users.noreply.github.com>
2020-11-13 00:24:55 +0700

    Add files via upload

---
+++ Assets/CustomId/CustomId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CustomId
+{
+    [SerializeField]
+    private string currentId;
+
+    public string CurrentId;
+
+    [SerializeField]
+    private CustomIdArray array;
+}
